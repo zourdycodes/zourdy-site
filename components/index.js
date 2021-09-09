fb260301ac64b2173b0225bebb57bf2997bb7d5d
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-09 13:03:40 +0700

    :tada: begin the project
    done configuring and clean up boilerplate

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const index = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['restcountries.eu'],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1Ijoiem91cmR5Y29kZXMiLCJhIjoiY2tzNWl5eWJ2MTc3MDMxcGl2M245cmQ4byJ9.at9VGulqiw_x2k3CqXNWrQ',
+  },
+};
