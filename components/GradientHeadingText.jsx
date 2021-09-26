707d7ec9d69772ff46955d591545a3df59efdce1
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-26 14:50:02 +0700

    :lipstick: improving styles
    checked

--- components/GradientHeadingText.jsx
+++ components/GradientHeadingText.jsx
@@ -1,7 +1,7 @@
 export default function GradientHeadingText({ text }) {
   return (
     <h2
-      className={` bg-gradient-to-r text-transparent to-red-800 from-pink-400 bg-clip-text text-4xl font-bold uppercase `}>
+      className={`bg-gradient-to-r text-transparent to-red-800 from-pink-400 bg-clip-text text-4xl font-bold uppercase `}>
       {text}
     </h2>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -10,54 +10,56 @@ export default function Header() {
     sm:px-6 md:px-8 max-w-3xl mx-auto">
       <div>
         <Link href="/">
-          <a className="text-gray-400 hover:text-red-400 text-3xl">Muhammad Zourdy</a>
+          <a className="bg-gradient-to-r text-transparent to-red-500 from-pink-400 bg-clip-text hover:from-red-400 hover:to-pink-600 text-3xl font-bold font-mono">
+            Muhammad Zourdy
+          </a>
         </Link>
       </div>
       <nav className="text-gray-100 text-lg mt-4 sm:mt-0">
         <ul>
-          <li className="inline-flex">
+          <li className="inline-flex pr-3 pl-3 border-r border-l border-gray-600">
             <Link href="/">
               <a
                 className={`${
                   pathname === "/"
-                    ? "text-red-400  hover:text-red-400"
-                    : "text-gray-100 hover:text-red-400 "
+                    ? "bg-gradient-to-r text-transparent to-pink-600 from-red-400 bg-clip-text  hover:from-red-400 hover:to-pink-600 font-mono font-bold "
+                    : "text-gray-100 hover:text-red-400 uppercase text-base font-mono"
                 }`}>
                 Home
               </a>
             </Link>
           </li>
-          <li className="inline-flex ml-3">
+          <li className="inline-flex  pr-3 pl-3 border-r border-gray-600">
             <Link href="/blog">
               <a
                 className={`${
                   pathname === "/blog"
-                    ? "text-red-400 hover:text-red-400"
-                    : "text-gray-100 hover:text-red-400 "
+                    ? "bg-gradient-to-r text-transparent to-pink-600 from-red-400 bg-clip-text  hover:from-red-400 hover:to-pink-600 font-mono font-bold"
+                    : "text-gray-100 hover:text-red-400 uppercase text-base font-mono"
                 }`}>
                 Blog
               </a>
             </Link>
           </li>
-          <li className="inline-flex ml-3">
+          <li className="inline-flex pr-3 pl-3 border-r border-gray-600">
             <Link href="/about">
               <a
                 className={`${
                   pathname === "/about"
-                    ? "text-red-400 hover:text-red-400"
-                    : "text-gray-100 hover:text-red-400 "
+                    ? "bg-gradient-to-r text-transparent to-pink-600 from-red-400 bg-clip-text  hover:from-red-400 hover:to-pink-600 font-mono font-bold"
+                    : "text-gray-100 hover:text-red-400 uppercase text-base font-mono"
                 }`}>
                 About
               </a>
             </Link>
           </li>
-          <li className="inline-flex ml-3">
+          <li className="inline-flex pr-3 pl-3 border-r border-gray-600">
             <Link href="/code">
               <a
                 className={`${
                   pathname === "/code"
-                    ? "text-red-400 hover:text-red-400"
-                    : "text-gray-100 hover:text-red-400 "
+                    ? "bg-gradient-to-r text-transparent to-pink-600 from-red-400 bg-clip-text  hover:from-red-400 hover:to-pink-600 font-mono font-bold"
+                    : "text-gray-100 hover:from-red-400 hover:to-pink-600 uppercase text-base font-mono"
                 }`}>
                 Snippets
               </a>
